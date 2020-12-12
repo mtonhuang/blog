4484b8da7db45abc7c339491fe04997c0c8535f2
v_mtonhuang <v_mtonhuang@tencent.com>
2020-12-12 19:57:22 +0800

    feat: del 无用文件夹

--- webpack4.0/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import "../css/common.less";
-import "../css/index.less";
-import "./flex.js"; //引入flex.js进行适配
-
-// 解决ios端伪类:active无效问题
-document.body.addEventListener('touchstart', function (){});
